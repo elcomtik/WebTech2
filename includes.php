14ff25b4477cbbe775f61325afd8f1e13f15ebd1
Adam Podhradsky <adoq111@gmail.com>
2017-05-08 20:18:41 +0200

    Initial commit contact.php

--- includes.php
+++ includes.php
@@ -5,6 +5,7 @@
  * Date: 5/6/17
  * Time: 9:59 PM
  */
+include_once 'session.php';
 ?>
 
 <meta charset="utf-8">
@@ -14,8 +15,4 @@
 <script rel="script" src="assets/jquery/jquery-3.2.1.min.js"></script>
 <script rel="script" src="assets/js/menu.js"></script>
 <script rel="script" src="assets/bootstrap/bootstrap/js/bootstrap.min.js"></script>
-
-<?php
-if (in_array("video.php", explode("/", $_SERVER['REQUEST_URI']))) {
-    echo "<script rel=\"script\" src=\"assets/js/video.js\"></script>";
-}
+<link rel="stylesheet" href="assets/font_awesome/css/font-awesome.min.css">
