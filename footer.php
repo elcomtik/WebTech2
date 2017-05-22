94d15a2d45909d575f6ad1b066bc8d82a8fd9136
Adam Podhradsky <adoq111@gmail.com>
2017-05-22 23:38:15 +0200

    footer fix columns

---
+++ footer.php
@@ -0,0 +1,58 @@
+
+<footer class="footer-distributed">
+
+    <div class="row">
+
+
+
+        <div class="footer-left col-lg-4">
+
+            <a href="http://is.stuba.sk/" class="footer-company-name">AIS STU</a><br>
+            <a href="http://aladin.elf.stuba.sk/rozvrh/" class="footer-company-name">Rozvrh hodín FEI</a><br>
+            <a href="http://elearn.elf.stuba.sk/moodle/" class="footer-company-name">Moodle FEI</a><br>
+
+            <a href="https://www.jedalen.stuba.sk/WebKredit" class="footer-company-name">Jedáleň STU</a><br>
+            <a href="https://webmail.stuba.sk/" class="footer-company-name">Webmail STU</a><br>
+        </div>
+
+
+
+        <div class="footer-center col-lg-4">
+
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>Ilkovičova 3</span> Bratislava, Slovensko</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p><span>+421 260 291 598, </span></p>
+                <p><span>+421 265 429 734</span></p>
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:katarina.kermietova@stuba.sk">katarina.kermietova@stuba.sk</a></p>
+            </div>
+
+        </div>
+
+        <div class="footer-right col-lg-4">
+
+            <a href="http://www.sski.sk/webstranka/" class="footer-company-name">SSKI</a><br>
+            <a href="https://kis.cvt.stuba.sk/i3/epcareports/epcarep.csp?ictx=stu&language=1" class="footer-company-name">Evidencia publikácií STU</a><br>
+            <a href="http://okocasopis.sk/" class="footer-company-name">Časopis OKO</a><br>
+
+            <div class="footer-icons">
+
+                <a href="https://www.facebook.com/UAMTFEISTU"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.youtube.com/channel/UCo3WP2kC0AVpQMIiJR79TdA"><i class="fa fa-youtube"></i></a>
+
+
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
