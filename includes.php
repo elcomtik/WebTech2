b1b243c6561c8cc03ee0e977ee2e2faa9d8595c3
Adam Podhradsky <adoq111@gmail.com>
2017-05-22 23:38:15 +0200

    footer fix columns

--- includes.php
+++ includes.php
@@ -9,15 +9,16 @@ include_once 'session.php';
 ?>
 
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="assets/bootstrap/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<script rel="script" src="assets/jquery/jquery-3.2.1.min.js"></script>
-<script rel="script" src="assets/js/menu.js"></script>
-<script rel="script" src="assets/bootstrap/bootstrap/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="assets/font_awesome/css/font-awesome.min.css">
-<link rel="stylesheet" href="assets/css/footer.css">
-<link rel="stylesheet" href="assets/css/footer_demo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script rel="script" src="assets/jquery/jquery-3.2.1.min.js"></script>
+    <script rel="script" src="assets/js/menu.js"></script>
+    <script rel="script" src="assets/bootstrap/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/font_awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+
+    <!--<link rel="stylesheet" href="assets/css/footer_demo.css">-->
 
 
 <?php
