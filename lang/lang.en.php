87b387582fa9d524403ea437ae22b2eb4544d160
Adam Podhradsky <adoq111@gmail.com>
2017-05-25 20:13:43 +0200

    updated contact

--- lang/lang.en.php
+++ lang/lang.en.php
@@ -32,5 +32,15 @@ $lang = array(
     'intranet_login.username' => "Username",
     'intranet_login.pass' => "Password",
     'intranet_login.login_button' => "Log in",
-    'video.video_list' => "List of videos"
+    'video.video_list' => "List of videos",
+    'contact.contact_person' => "Contact person",
+    'contact.locate_us' => "Locate us",
+    'contact.faculty' => "Faculty of Electrical Engineering and Information Technology",
+    'contact.university' => "Slovak University of Technology",
+    'contact.country' => "Slovak republic",
+    'footer.dining_room' => "Jedáleň STU",
+    'footer.timetable' => "Timetable FEI",
+    'footer.country' => "Bratislava, Slovakia",
+    'footer.magazine' => "Magazine OKO",
+    'footer.evidence' => "Publication evidence STU"
 );
