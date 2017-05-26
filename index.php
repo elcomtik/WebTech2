33d128e46f38b3b266ca203ec81ef53d01c3f9c7
Adam Podhradsky <adoq111@gmail.com>
2017-05-26 22:32:37 +0200

    edit banners

--- index.php
+++ index.php
@@ -28,21 +28,15 @@ include_once 'menu.php';
 <div class="slideshow-container">
 
     <div class="mySlides fade_slide">
-        <div class="numbertext">1 / 3</div>
-        <img src="img1.jpg" style="width:100%">
-        <div class="text">Caption Text</div>
+        <div class="numbertext">1 / 2</div>
+        <img src="uploads/banners/banner1.jpg" style="width:100%">
+        <div class="text">Popis</div>
     </div>
 
     <div class="mySlides fade_slide">
-        <div class="numbertext">2 / 3</div>
-        <img src="img2.jpg" style="width:100%">
-        <div class="text">Caption Two</div>
-    </div>
-
-    <div class="mySlides fade_slide">
-        <div class="numbertext">3 / 3</div>
-        <img src="img3.jpg" style="width:100%">
-        <div class="text">Caption Three</div>
+        <div class="numbertext">2 / 2</div>
+        <img src="uploads/banners/banner2.jpg" style="width:100%">
+        <div class="text">Popis 2</div>
     </div>
 
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
