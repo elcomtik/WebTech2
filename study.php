3fa5f6f905d6befb229890fbb8ac32a87244f129
Adam Podhradsky <adoq111@gmail.com>
2017-05-26 19:49:42 +0200

    added page study.php

---
+++ study.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eentox
+ * Date: 5/5/17
+ * Time: 7:13 PM
+ */
+?>
+
+<head>
+    <?php include_once 'includes.php'; ?>
+    <title>Domov</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/slideshow.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<?php
+include_once 'session.php';
+?>
+
+<body>
+<?php
+include_once 'menu.php';
+?>
+
+<h3 class="mojnadpish3">Štúdium na našom ústave</h3>
+
+<h4 class="mojnadpish3">Pre uchádzačov o štúdium</h4>
+
+<h5 class="mojnadpish3">Bakalárske štúdium</h5>
+<p>Informácie budú dodané neskôr.</p>
+Kompletný študijný plán pre akademický rok 2017-2018 (SP20172018b.pdf).<br/>
+Ďalšie informácie na http://www.mechatronika.cool
+
+
+<h5 class="mojnadpish3">Inžinierske štúdium</h5>
+Prečo študovať na našom ústave?
+<ul>
+    <li>možnosť získať znalosti, ktoré sú implementovateľné v praxi</li>
+    <li>menšie skupiny študentov</li>
+    <li>možnosť dohodnúť si tému pre diplomovku s vybraným pedagógom na základe vlastných preferencií</li>
+    <li>možnosť riešiť diplomovú prácu a teda to, čo každého zaujíma, až 3 semestre</li>
+    <li>pre vynikajúcich študentov možnosť študovať dištančnou metódou</li>
+    <li>pre absolventov bakalárskeho štúdia na FEI STU odpustená prijímacia skúška</li>
+    <li>snaha o maximálnu informovanosť študentov prostredníctvom web stránky v dostatočnom predstihu</li>
+</ul>
+
+Nebudem mať problémy, keď som neštudoval mechatroniku aj na bakalárskom štúdiu?
+<ul>
+    <li>Mechatronika predstavuje medziodborové štúdium, takže každý by sa tu mal nájsť. Hneď v prvom semestri
+        inžinierskeho štúdia je pre študentov, ktorí predtým neštudovali mechatroniku pripravený vyrovnávací predmet z
+        oblasti automatizácie.
+    </li>
+</ul>
+
+<h5>Študijný program – 1. ročník</h5>
+Zimný semester
+<ul>
+    <li>CAE mechatronických systémov - tvorba virtuálnych dynamických modelov a ich simulácia</li>
+    <li>Metóda konečných prvkov - modelovanie a analýza mechatronických prvkov a systémov</li>
+    <li>Optimalizácia procesov v mechatronike – optimalizačné úlohy a metódy v inžinierskych aplikáciách</li>
+    <li>Vývojové programové prostredia pre mechatronické systémy - programovanie mikroprocesorov</li>
+    <li>Povinne voliteľný predmet</li>
+</ul>
+Letný semester
+<ul>
+    <li>Diplomový projekt 1</li>
+    <li>Metódy číslicového riadenia – návrh regulačných obvodov pre modely mechatronických systémov</li>
+    <li>Multifyzikálne procesy v mechatronike - modelovanie tepelných, termoelastických, termoelektrických a piezoelektrických systémov</li>
+    <li>Pokročilé informačné technológie - klient-server aplikácie, riadenie mechatronických systémov v prostredí internetu, Internet vecí (IoT), Industry 4.0</li>
+    <li>Povinne voliteľný predmet</li>
+</ul>
+
+Možné PVP pre záujemcov o elektroniku
+<ul>
+    <li>Inteligentné mechatronické systémy
+        <ul>
+            <li>implementácia metód výpočtovej a umelej inteligencie pre mechatronické systémy</li>
+        </ul>
+    </li>
+    <li>MEMS - inteligentné senzory a aktuátory
+        <ul><li>najmodernejšie senzory používané nielen v automobilovom priemysle (akcelerometre, gyroskopy, CCD senzory) a spracovanie signálov vnorenými mikropočítačmi</li></ul>
+    </li>
+</ul>
+
+<h5>Možné PVP pre záujemcov o automobily</h5>
+<ul>
+    <li>Transmisné systémy automobilov a elektromobilov
+        <ul><li>prevodové mechanizmy automobilov a elektromobilov</li></ul>
+    <li>Pohonné systémy a zdroje v elektromobiloch
+        <ul><li>modelovanie a simulovanie činnosti trakčného a energetického systému elektromobilu</li></ul>
+</ul>
+
+<h5>Možné PVP pre záujemcov o informatiku</h5>
+<ul>
+    <li>Inteligentné mechatronické systémy
+        <ul><li>implementácia metód výpočtovej a umelej inteligencie pre mechatronické systémy</li></ul>
+    </li>
+    <li>Vybrané kapitoly z automatického riadenia pre mechatroniku
+        <ul><li>vyrovnávací predmet z automatizácie</li></ul>
+    </li>
+    <li>MEMS - inteligentné senzory a aktuátory
+        <ul><li>najmodernejšie senzory používané nielen v automobilovom priemysle (akcelerometre, gyroskopy, CCD senzory) a spracovanie signálov vnorenými mikropočítačmi</li></ul>
+    </li>
+</ul>
+
+
+
+
+<!----------------------------------------------------------------------------------------------------------->
+
+
+
+<?php include_once 'footer.php' ?>
+</body>
