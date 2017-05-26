81b8a049e2ba76df3a0b60f7290bc914fb65656d
Roman Danko <mail@romandanko.sk>
2017-05-26 15:25:13 +0200

    fix pre margin stranky

--- gallery.php
+++ gallery.php
@@ -30,60 +30,62 @@ include_once 'menu.php';
 include_once 'DB_config.php';
 
 ?>
-<div class="container-fluid col-lg-3">
-    <table class="table table-hover col-lg-12">
-        <thead>
-        <th>
-            <?php echo $lang['galery.galery_list']; ?>
-        </th>
-        </thead>
-        <tbody>
+<div class="row row-no-margin">
+    <div class="container-fluid col-lg-3">
+        <table class="table table-hover col-lg-12">
+            <thead>
+            <th>
+                <?php echo $lang['galery.galery_list']; ?>
+            </th>
+            </thead>
+            <tbody>
 
-        <?php
-        $conn = new mysqli($servername, $username, $password, $db_name);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $conn->set_charset('utf8');
-
-        $stmt =  $conn->stmt_init();
-        if ($stmt = $conn->prepare("SELECT id,date,title_sk,title_en,folder FROM GALERY")){
-
-            $stmt->execute();
-
-            /* bind result variables */
-            $stmt->bind_result($id, $date, $title_sk, $title_en, $folder);
-            while ($stmt->fetch()) {
-
-                echo "<tr folder=" . $folder . "><td>";
-
-                if ($_SESSION['lang'] == "en"){
-                    echo $title_en;
-                }
-                elseif ($_SESSION['lang'] == "sk"){
-                    echo $title_sk;
-                }
-                echo "</td></tr>";
+            <?php
+            $conn = new mysqli($servername, $username, $password, $db_name);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
             }
-            $stmt->close();
-        }
-        else{
-            echo "false";
-        }
+            $conn->set_charset('utf8');
 
-        $conn->close();
-        ?>
+            $stmt =  $conn->stmt_init();
+            if ($stmt = $conn->prepare("SELECT id,date,title_sk,title_en,folder FROM GALERY")){
 
-        </tbody>
-    </table>
-</div>
+                $stmt->execute();
 
-<div class="container-fluid col-lg-9">
-    <div class="demo-gallery">
-        <ul id="lightgallery" class="list-unstyled row">
-        </ul>
+                /* bind result variables */
+                $stmt->bind_result($id, $date, $title_sk, $title_en, $folder);
+                while ($stmt->fetch()) {
+
+                    echo "<tr folder=" . $folder . "><td>";
+
+                    if ($_SESSION['lang'] == "en"){
+                        echo $title_en;
+                    }
+                    elseif ($_SESSION['lang'] == "sk"){
+                        echo $title_sk;
+                    }
+                    echo "</td></tr>";
+                }
+                $stmt->close();
+            }
+            else{
+                echo "false";
+            }
+
+            $conn->close();
+            ?>
+
+            </tbody>
+        </table>
+    </div>
+
+    <div class="container-fluid col-lg-9">
+        <div class="demo-gallery">
+            <ul id="lightgallery" class="list-unstyled row">
+            </ul>
+        </div>
     </div>
 </div>
 
-<?php //include_once('footer.php');?>
+<?php include_once('footer.php');?>
 </body>
